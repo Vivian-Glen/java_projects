110036f0b193a28f72d72e94239b83eedc4ae95f
Vivian-Glen <138390987+Vivian-Glen@users.noreply.github.com>
2024-01-02 06:59:30 -0500

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,29 @@
+public class Calculator{
+  public Calculator()
+  {
+
+  }
+  public int add(int a , int b){
+   return a+b;
+  }
+  public int subtract(int a, int b){
+    return a-b;
+  }
+  public int multiply(int a , int b){
+    return a*b;
+  }
+  public int divide(int a, int b){
+    return a/b;
+  }
+  public int modulo(int a, int b){
+    return a%b;
+  }
+  public static void main(String[] args ){
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5,7));
+    System.out.println(myCalculator.subtract(45,11));
+    System.out.println(myCalculator.multiply(2,6));
+    System.out.println(myCalculator.divide(8,2));
+    System.out.println(myCalculator.modulo(7,3));
+  }
+}
